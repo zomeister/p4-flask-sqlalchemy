1d6d70110c602bcdab3c96128fd12815fe71af21
Ben Botsford <ben.botsford@flatironschool.com>
2022-09-06 15:31:11 -0500

    remove solution

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,18 +1,8 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-from models import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-migrate = Migrate(app, db)
-
-db.init_app(app)
 
 if __name__ == '__main__':
     app.run()
