69f9070819995f5f0c5427236bd865e62b760c9a
Zoe Guidry <oyezoey@gmail.com>
2023-07-29 16:30:42 -0500

    lab completed

--- server/app.py
+++ server/app.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db' # Path to the database file (instead of definign this path in the 'alembic.ini' file)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -6,3 +6,20 @@ metadata = MetaData(naming_convention={
 })
 
 db = SQLAlchemy(metadata=metadata)
+
+class Owner(db.Model):
+    __tablename__ = 'owners'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    pets = db.relationship('Pet', backref='owner')
+    def __repr__(self):
+        return f'<Pet Owner {self.name}>'
+    
+class Pet(db.Model):
+    __tablename__ = 'pets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
+    def __repr__(self):
+        return f'<Pet {self.name}, {self.species}>'
